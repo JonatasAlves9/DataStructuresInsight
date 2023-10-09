60b0cd7f26afa675c7ecbdac6306094c427af09f
JonatasAlves9 <alvesjonatas99@gmail.com>
2023-10-09 12:47:27 -0300

    feat: create method delete in linkedLists

--- LinearDataStructures/LinkedLists/example-typescript.ts
+++ LinearDataStructures/LinkedLists/example-typescript.ts
@@ -10,20 +10,41 @@ class ListNode<T> {
 class LinkedList<T> {
     head: ListNode<T> | null = null;
 
-    append(value: T):void {
+    append(value: T): void {
         const newNode = new ListNode(value);
-        if (this.head === null){
+        if (this.head === null) {
             this.head = newNode
             return
         }
 
         let current = this.head;
-        while (current.next !== null){
+        while (current.next !== null) {
             current = current.next
         }
 
         current.next = newNode
 
+    }
+
+    delete(value: T): void {
+        if (this.head && this.head.value === value) {
+            this.head = this.head.next;
+            return;
+        }
+
+        let current = this.head;
+        let previousCurrent: ListNode<T> | null = null;
+
+        while (current !== null && current.value !== value) {
+            previousCurrent = current;
+            current = current.next;
+        }
+
+        if (current !== null) {
+            if (previousCurrent !== null) {
+                previousCurrent.next = current.next;
+            }
+        }
     }
 
     print(): void {
@@ -36,7 +57,11 @@ class LinkedList<T> {
 }
 
 const list = new LinkedList<number>();
+console.log('Teste de append:')
 list.append(1);
 list.append(2);
 list.append(3);
+list.print(); // Isso imprimirá: 1, 2, 3
+console.log('Teste de Delete: ')
+list.delete(2)
 list.print(); // Isso imprimirá: 1, 2, 3
